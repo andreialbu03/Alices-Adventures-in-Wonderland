157d538974f5a2db5bd59b76f4274eaa6957de82
Andrei Albu <andrei.albu03@gmail.com>
2022-09-10 13:49:22 -0400

    docs: document inventory class

--- Inventory.java
+++ Inventory.java
@@ -1,34 +1,52 @@
+/**
+ * title: Inventory.java
+ * description: Class representing the inventory of the player
+ * date: Sep 10, 2022
+ * @author Andrei Albu
+ * @version 1.0
+ * @copyright 2022 Andrei Albu
+ */
+
+// Import Java packages
 import java.util.*;
 
+/** Primary (public) class Inventory */
 public class Inventory {
 
-    private static Map<String, Item> inventory;
+    /** Instance variable to store the items in the inventory */
+    private Map<String, Item> inventory;
 
+    /** Constructor for the Inventory class */
     public Inventory() {
         inventory = new HashMap<String, Item>();
     }
 
-    // TODO implement get number of items in inventory
+    /** Public method to return the number of items in the inventory */
     public int getSize() {
         return inventory.size();
     }
 
+    /** Public method to add an item to the inventory */
     public void putItem(String name, Item item) {
         inventory.put(name, item);
     }
 
+    /** Public method to check whether an item exists in the inventory */
     public boolean contains(String name) {
         return inventory.containsKey(name);
     }
 
+    /** Public method to remove an item from the inventory */
     public void remove(String name) {
         inventory.remove(name);
     }
 
+    /** Public method to return an item from the inventory */
     public Item getItem(String name) {
         return inventory.get(name);
     }
 
+    /** Public method to return the inventory (returns the keys) */
     public Set<String> getItemsSet() {
         return inventory.keySet();
     }
