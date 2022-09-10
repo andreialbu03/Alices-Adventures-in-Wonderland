ef44e6460aabff8e1a2b988f0f33920f981ba23a
Andrei Albu <andrei.albu03@gmail.com>
2022-09-10 13:35:53 -0400

    docs: document control class

--- Control.java
+++ Control.java
@@ -1,18 +1,35 @@
+/**
+ * title: Control.java
+ * description: Class responsible for validating user input
+ * date: Sep 10, 2022
+ * @author Andrei Albu
+ * @version 1.0
+ * @copyright 2022 Andrei Albu
+ */
+
+// Import Java packages
 import java.util.*;
 
+/** Primary (public) class Control */
 public class Control {
 
+    /** Public static method to check whether or not an action is valid */
     public static String checkInput(String command, HashMap<String, Actions> validActions) {
 
+        // Iterate over each valid action in the HashMap
         for (String action : validActions.keySet()) {
 
+            // Iterate over each valid action in the array
             for (String actionAlias : validActions.get(action).getActions()) {
+
+                // If the user input matches a valid action, return the action
                 if (actionAlias.equals(command)) {
                     return action;
                 }
             }
         }
 
+        // If the user input does not match a valid action, return null
         return null;
     }
 
