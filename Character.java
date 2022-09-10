c35e0904a29ad889f897d83f7a59065321101ecb
Andrei Albu <andrei.albu03@gmail.com>
2022-09-10 12:54:44 -0400

    docs: document character class

--- Character.java
+++ Character.java
@@ -1,29 +1,47 @@
+/**
+ * title: Character.java
+ * description: Class that represents a non-playable character (NPC) in the game
+ * date: Sep 10, 2022
+ * @author Andrei Albu
+ * @version 1.0
+ * @copyright 2022 Andrei Albu
+ */
+
+// Import Java packages
 import java.util.*;
 
+/** Primary (public) class Character */
 public class Character {
 
+    /** Instance variables storing attributes of an NPC */
     private String name;
     private String dialog;
     private String location;
 
+    /** Constructor for the Character class, accepts 3 parameters */
     public Character(String name, String dialog, String location) {
+        // Initializes instance variables
         this.name = name;
         this.dialog = dialog;
         this.location = location;
     }
 
+    /** Public method that outputs the NPC's dialog */
     public void talk() {
         System.out.print("\n" + dialog);
     }
 
+    /** Public getter method that returns the NPC's name */
     public String getName() {
         return name;
     }
 
+    /** Public getter method that returns the NPC's dialog */
     public String getDialog() {
         return dialog;
     }
 
+    /** Public getter method that returns the NPC's location */
     public String getLocation() {
         return location;
     }
