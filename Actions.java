5e3aeebb2e217f83e2a56486e580a1b99bd37f5a
Andrei Albu <andrei.albu03@gmail.com>
2022-09-10 12:47:45 -0400

    docs: document actions class

--- Actions.java
+++ Actions.java
@@ -1,10 +1,25 @@
+/**
+ * title: Actions.java
+ * description: Class that stores the valid actions that can be performed in the game
+ * date: Sep 10, 2022
+ * @author Andrei Albu
+ * @version 1.0
+ * @copyright 2022 Andrei Albu
+ */
+
+/** Primary (public) class Actions */
 public class Actions {
+
+    /** Instance variable to keep track of valid actions */
     private String[] actions;
 
+    /** Constructor for Actions class accepting a String array */
     public Actions(String[] actions) {
+        // Initializes instance variable
         this.actions = actions;
     }
 
+    // Getter method to return the valid actions
     public String[] getActions() {
         return actions;
     }
